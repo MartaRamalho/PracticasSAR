9858be95b80b1811d23af61309932423d0022fb9
MartaRamalho <martaramalho512@gmail.com>
2023-03-21 20:00:00 +0100

    Finished practice

--- practica3/SAR_p3_monkey_lib.py
+++ practica3/SAR_p3_monkey_lib.py
@@ -36,33 +36,59 @@ class Monkey():
         return self.info.get('n', 0)
 
     def index_sentence(self, sentence: str):
+        """
+        Este método analiza la frase pasada por parámetro separándola en los diferentes n-gramas y 
+        añadiendola al modelo.
+
+        :param 
+            sentence: frase a analizar y añadir al modelo
+        """
         n = self.info['n']
         for i in range(2, n+1):
+            # añadimos antes de la frase un número de dólares según el n-grama a considerar
+            # por ejemplo: para los bigramas, añadimos 1 $ y para los trigramas añadimos 2 $, etc.
             sentenceN = '$ '*(i-1) + sentence
             words = sentenceN.split()
+            # creamos un bucle que asigne a la variable w los diferentes valores de posiciones del vector words
             for w in range(len(words)):
+                # comprobamos que si sumamos i a la posición w, este no es mayor que el tamaño del vector de palabras
                 if w+i <= len(words):
+                    # creamos una tupla con la palabra en posición w y las i-2 palabras siguientes
                     ngram = tuple(words[w:w+i-1])
+                    # añadimos la tupla a info con un diccionario vacio si no existe
                     self.info['lm'][i][ngram] = {
                     } if ngram not in self.info['lm'][i] else self.info['lm'][i][ngram]
+                    # se suma uno al contador de veces que aparece la palabra después de la tupla obtenida
                     countWord = 1 if words[w +
                                            i-1] not in self.info['lm'][i][ngram] else self.info['lm'][i][ngram][words[w+i-1]]+1
                     self.info['lm'][i][ngram][words[w+i-1]] = countWord
-        print(self.info)
 
     def compute_lm(self, filenames: List[str], lm_name: str, n: int):
+        """
+        Este método separa un archivo por frases y llama al método index_sentence con cada una de
+        ellas
+
+        :param 
+            filenames: lista de archivos a procesar
+            lm_name: nombre del archivo .lm destino
+            n: número de n-gramas a considerar
+        """
         self.info = {'name': lm_name, 'filenames': filenames, 'n': n, 'lm': {}}
         for i in range(2, n+1):
             self.info['lm'][i] = {}
-
         for filename in filenames:
-            sentences = self.r1.split(open(filename, encoding='utf-8').read())
+            # separamos el archivo en frases, las cuales están separadas por signos de puntuación o doble salto de línea
+            sentences = self.r1.split(
+                open(filename, encoding='utf-8').read().replace('\n\n', '.'))
             for line in sentences:
-                s = line.split('\n\n')
-                for l in s:
-                    if l != '':
-                        l = self.r2.sub(" ", l).lower()+' $'
-                        self.index_sentence(l)
+                # eliminamos los signos indeseados de la frase y la pasamos a minuscula
+                line = self.r2.sub(" ", line).lower()
+                # comprobamos que no sea una frase vacía, y si lo es, saltamos a la siguiente iteración
+                if not line.strip():
+                    continue
+                # si no lo es, añadimos el $ final y llamamos al método index_sentence
+                line = line+' $'
+                self.index_sentence(line)
         for i in range(2, n+1):
             convert_to_lm_dict(self.info['lm'][i])
 
@@ -100,30 +126,48 @@ class Monkey():
                     f"'{' '.join(prev)}'\t=>\t{wl[0]}\t=>\t{', '.join(['%s:%s' % (x[1], x[0]) for x in wl[1]])}", file=fh)
 
     def generate_sentences(self, n: Optional[int], nsentences: int = 10, prefix: Optional[str] = None):
+        """
+        Este método generea frases aleatorias a partir de un archivo .lm que contiene
+        el lenguaje.
+
+        :param 
+            n: número que indica qué n-grama utilizar para generar las frases
+            nsentences: número de frases que se desea generar
+            prefix: prefijo que se desea tener en todas las frases
+        """
         if not n:
             n = self.get_n()
         if prefix:
             prefLen = len(prefix.split())
+            # si la longitud del prefijo es menor o igual que el tamaño de las tuplas,
+            # se añaden n $ hasta que la longitud coincida con la de la tupla
             if prefLen <= n-1:
                 prefixAux = "$ "*(n-prefLen-1)+prefix
+            # si el prefijo es demasiado largo, se cojerán solo las n-1 últimas palabras para formar la tupla
             else:
                 prefixAux = ' '.join(prefix.split()[prefLen-n+1:])
+        # si no se ha elegido un prefijo o éste no existe en info, entonces se empezará a formar las frases por la tupla de n-1 $
         if not prefix or tuple(prefixAux.split()) not in self.info['lm'][n]:
             prefixAux = '$ '*(n-1)
         for sentence in range(nsentences):
             thisTuple = tuple(prefixAux.split())
             last = ''
-            sentence = prefix
+            sentence = prefix if prefix else ''
             word = ''
+            # mientras que la última palabra procesada no sea $ y la frase tenga menos de 50 palabras
+            # se obtiene un numero aleatorio y se elige la palabra sucesora de la tupla en la que cae dicho número aleatorio
             while '$' not in last and len(sentence.split()) != 50:
                 max = self.info['lm'][n][thisTuple][0]
                 index = random.randint(1, max)
                 sumWeight = 0
                 for weight, word in self.info['lm'][n][thisTuple][1]:
                     sumWeight += weight
+                    # cuando alcanzamos el número aleatorio generado, comprobamos si la palabra elegida es un $ o no,
+                    # si lo es, terminamos esta frase y empezamos otra
                     if sumWeight >= index:
                         if word == '$':
                             break
+                        # en caso contrario, añadimos la palabra a la frase y generamos la nueva tupla con esta palabra
                         sentence += " "+word
                         thisTuple = thisTuple[-(n-2):] + \
                             (word, ) if n > 2 else (word, )
